d5c68c7621fe70a3a262b68ff83ad1857f31f8d6
Kevin FERRIER <13919713+kevin-ferrier@users.noreply.github.com>
2024-08-21 15:13:26 +0200

    movements

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from player import Player
 from constants import *
 import pygame
 
@@ -6,15 +7,31 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while playing:
         # event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        for up_objects in updatable:
+            up_objects.update(dt)
+
         # display
         screen.fill("black")
+        for draw_object in drawable:
+            draw_object.draw(screen)
+
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
